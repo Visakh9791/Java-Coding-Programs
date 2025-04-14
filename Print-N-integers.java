4a0ef6f3b9ea5ca6a9214d1de8942979d42d2383
Visakh .S <161792899+Visakh9791@users.noreply.github.com>
2025-04-14 20:03:18 +0530

    Create Print-N-integers.java

---
+++ Print-N-integers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Integers{
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        for(int i=1;i<n+1;i++){
+            System.out.println(i);
+        }
+    }
+}
