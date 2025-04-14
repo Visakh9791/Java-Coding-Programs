3ab2b24003f4b08ce1a0f944e8d7b71b9e0449c5
Visakh .S <161792899+Visakh9791@users.noreply.github.com>
2025-04-14 22:00:38 +0530

    Create SumOfEvenNumFromMtoN.java

---
+++ SumOfEvenNumFromMtoN.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class SumOfEvenNumFromMtoN{
+    public static void main(String[] args){
+        Scanner obj =new Scanner(System.in);
+        int m = obj.nextInt();
+        int n = obj.nextInt();
+        int sum=0;
+        for(int i=m;i<n+1;i++){
+            if(i%2 == 0){
+                sum=sum+i;    
+            }
+            
+        }
+        System.out.println(sum);
+    }
+}
